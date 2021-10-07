dd38579072c739413785f6bfd2816b3cb8761cfb
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-10-07 22:59:06 +0200

    Adds Lottery Rinkeby deploy script

---
+++ lottery/deploy.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.INFURA_RINKEBY_API
+);
+
+const web3 = new Web3(provider);
+
+const deploy  = async () => {
+  // A mnemonic can generate many accounts, but we use only the first account to deploy the contract.
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account:', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+
+    // Using Wei unit
+    .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+  console.log('Contract deployed to:', result.options.address);
+};
+
+deploy();
