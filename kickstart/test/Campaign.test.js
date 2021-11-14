5c114d5de6bc2c0ed052ff67357dd81906ee3a43
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-11-14 16:14:17 +0100

    Adds Campaign and CampaignFactory tests

---
+++ kickstart/test/Campaign.test.js
@@ -0,0 +1,112 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+// We require the already compiled files
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+const minimumContribution = '100';
+let gas = '1000000';
+
+beforeEach(async() => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: gas });
+
+  await factory.methods.createCampaign(minimumContribution).send({
+    from: accounts[0],
+    gas: gas
+  });
+
+  // We get the first element returned by the array
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  // The campaign contract has already been deploy and we have the address
+  // Now we need a JS representation of that deployed contract
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('allows people to contribute money and marks them as approvers', async() => {
+    await campaign.methods.contribute().send({
+      value: minimumContribution + 1,
+      from: accounts[1]
+    });
+
+    // We can get the value directly from the mapper instead of all the mapper 
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: minimumContribution / 2,
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries', '250', accounts[1])
+      .send({ from: accounts[0], gas: gas });
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal('Buy batteries', request.description);
+  });
+
+  it('processed requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods
+      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas: gas });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: gas
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: gas
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]); // Returns Wei string
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance); // String to decimal
+
+    // The balance is not reset in between tests
+    // That is a ganache limitation, so we can assume it starts with 99
+    assert(balance > 104);
+  });
+});
+
