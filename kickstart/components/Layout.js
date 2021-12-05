242f615f0a6e34bf0a95f690971035bfe881c137
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-05 14:26:49 +0100

    Adds Campaign React files

---
+++ kickstart/components/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
+import Header from './Header';
+
+const Layout = (props) => {
+  return (
+    <Container>
+      <Head>
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+      </Head>
+      
+      <Header />
+      {props.children}
+    </Container>
+  )
+};
+
+export default Layout;
