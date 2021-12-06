66313af8fd0bb629816a2f44be005c507c074856
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-06 13:00:19 +0100

    Adds WavePortal base contract

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+// Include some hardhat utils, like console.log
+import "hardhat/console.sol";
+
+contract WavePortal {
+	constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
