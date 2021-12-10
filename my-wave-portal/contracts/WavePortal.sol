18893d5dfca14d28aa42565bc2c6492d82c38750
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-10 17:16:37 +0100

    Funds contract and sends a bit of eth to every waver

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -26,18 +26,26 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
+    constructor() payable {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
     function wave(string memory _message) public {
-        totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
+		totalWaves += 1;
+		console.log("%s has waved!", msg.sender);
 
-        waves.push(Wave(msg.sender, _message, block.timestamp));
+		waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        emit NewWave(msg.sender, block.timestamp, _message);
-    }
+		emit NewWave(msg.sender, block.timestamp, _message);
+
+		uint256 prizeAmount = 0.0001 ether;
+		require(
+			prizeAmount <= address(this).balance,
+			"Trying to withdraw more money than the contract has."
+		);
+		(bool success, ) = (msg.sender).call{value: prizeAmount}("");
+		require(success, "Failed to withdraw money from contract.");
+	}
 
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
