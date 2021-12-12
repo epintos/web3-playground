1d0b4bf989d2f600c0451fce64743abb437630f1
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-12 20:27:21 +0100

    Adds harhat library to verify smart contract

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -1,4 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-etherscan");
+
 require('dotenv').config();
 
 module.exports = {
@@ -14,4 +16,9 @@ module.exports = {
 			// 	accounts: [process.env.PRIVATE_KEY],
 			// },
 		},
+    etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: process.env.ETHERSCAN_API_KEY,
+    }
   };
