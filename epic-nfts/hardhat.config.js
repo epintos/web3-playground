fbd9a439345e8dc8f953c9849fd8fc8618a89136
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-12 19:14:50 +0100

    Replaces NFT encoded in Base64

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -1,11 +1,17 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
-  solidity: '0.8.0',
-  networks: {
-    rinkeby: {
-      url: 'YOUR_ALCHEMY_API_URL',
-      accounts: ['YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY'],
-    },
-  },
-};
+	solidity: '0.8.0',
+	networks: {
+			rinkeby: {
+				url: process.env.ALCHEMY_API_URL,
+				accounts: [process.env.RINKEBY_ACCOUNT_KEY], // Private key from Metamask. We need it to deploy the contract
+			},
+			// mainnet: {
+			// 	chainId: 1,
+			// 	url: process.env.PROD_ALCHEMY_API_URL,
+			// 	accounts: [process.env.PRIVATE_KEY],
+			// },
+		},
+  };
