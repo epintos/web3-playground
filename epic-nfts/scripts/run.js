a6d55bf009d53a394753c7537d269ad3deaace85
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-12 17:45:05 +0100

    Adds function to mint an NFT

--- epic-nfts/scripts/run.js
+++ epic-nfts/scripts/run.js
@@ -3,6 +3,17 @@ const main = async () => {
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function.
+  let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
+  // Mint another NFT for fun.
+  txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+
 };
 
 const runMain = async () => {
