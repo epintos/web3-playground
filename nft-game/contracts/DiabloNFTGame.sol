6a481b868031f957f1ce5984232da8bc9b587eb2
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-14 14:49:33 +0100

    Adds mint function for character

---
+++ nft-game/contracts/DiabloNFTGame.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// Helper to encode in Base64
+import "./libraries/Base64.sol";
+
+// Our contract inherits from ERC721, which is the standard NFT contract!
+contract DiabloNFTGame is ERC721 {
+  // We'll hold our character's attributes in a struct.
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;        
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
+  }
+
+  // The tokenId is the NFTs unique identifier, it's just a number that goes
+  // 0, 1, 2, 3, etc.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // An array with each characters defaults attributes
+  // This will be helpful when we mint new characters and need to know
+  // things like their HP, AD, etc.
+  CharacterAttributes[] defaultCharacters;
+
+  // We create a mapping from the nft's tokenId => that NFTs attributes.
+  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+  // A mapping from an address => the NFTs tokenId. Gives me an easy way
+  // to store the owner of the NFT and reference it later.
+  mapping(address => uint256) public nftHolders;
+
+  // Data passed in to the contract when it's first created initializing the characters.
+  // We're going to actually pass these values in from from run.js.
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterAttackDmg
+  )
+    ERC721("Diablo", "DIABLO")
+  {
+    // Loop through all the characters, and save their values in our contract so
+    // we can use them later when we mint our NFTs.
+    for(uint i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i]
+      }));
+
+      CharacterAttributes memory c = defaultCharacters[i];
+
+      // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
+      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+
+    // I increment tokenIds here so that my first NFT has an ID of 1.
+    }
+    
+    _tokenIds.increment();
+  }
+
+  // Users would be able to hit this function and get their NFT based on the
+  // characterId they send in!
+  function mintCharacterNFT(uint _characterIndex) external {
+    // Get current tokenId (starts at 1 since we incremented in the constructor).
+    uint256 newItemId = _tokenIds.current();
+
+    // Assigns the tokenId to the caller's wallet address.
+    _safeMint(msg.sender, newItemId);
+
+    // We map the tokenId => their character attributes.
+    nftHolderAttributes[newItemId] = CharacterAttributes({
+      characterIndex: _characterIndex,
+      name: defaultCharacters[_characterIndex].name,
+      imageURI: defaultCharacters[_characterIndex].imageURI,
+      hp: defaultCharacters[_characterIndex].hp,
+      maxHp: defaultCharacters[_characterIndex].maxHp,
+      attackDamage: defaultCharacters[_characterIndex].attackDamage
+    });
+
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+    
+    // Keep an easy way to see who owns what NFT.
+    nftHolders[msg.sender] = newItemId;
+
+    // Increment the tokenId for the next person that uses it.
+    _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+    string memory json = Base64.encode(
+      bytes(
+        string(
+          abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Diablo 2!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,'} ]}'
+          )
+        )
+      )
+    );
+
+    string memory output = string(
+      abi.encodePacked("data:application/json;base64,", json)
+    );
+    
+    return output;
+  }
+}
--- nft-game/scripts/run.js
+++ nft-game/scripts/run.js
@@ -1,5 +1,5 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory('MyNFTGame');
+  const gameContractFactory = await hre.ethers.getContractFactory('DiabloNFTGame');
   const gameContract = await gameContractFactory.deploy(
     ["Barbarian", "Sorcerer", "Paladin"],       // Names
     ["https://i1.wp.com/gamerhabitat.com/wp-content/uploads/2021/08/barbariand2r.jpg?fit=1200%2C675&ssl=1", // Images
@@ -10,6 +10,17 @@ const main = async () => {
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  // Get the value of the NFT's URI.
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
+
 };
 
 const runMain = async () => {
