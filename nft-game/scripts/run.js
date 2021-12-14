5a6d97759f121b39ccd93f04339d1d7020175333
Esteban Pintos <esteban.pintos@wetransfer.com>
2021-12-14 14:27:21 +0100

    Initializes characters in the contract

--- nft-game/scripts/run.js
+++ nft-game/scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyNFTGame');
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Barbarian", "Sorcerer", "Paladin"],       // Names
+    ["https://i1.wp.com/gamerhabitat.com/wp-content/uploads/2021/08/barbariand2r.jpg?fit=1200%2C675&ssl=1", // Images
+    "https://www.dexerto.com/wp-content/uploads/2021/08/12/diablo-2-sorceress-guide.jpeg", 
+    "https://www.pcgamesn.com/wp-content/uploads/2021/09/diablo-2-resurrected-paladin-builds-best.jpg"],
+    [100, 220, 150],                    // HP values
+    [200, 80, 150]                       // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
